3e931337237ab4119fb111bfed67e0aa514ff365
SuperMaciej <ansim11@wp.pl>
2021-01-11 06:46:28 +0100

    Z plikami html, js, css

---
+++ javas.js
@@ -0,0 +1,97 @@
+function energiaMinuty() {
+   		var liczbaPole_1 = document.getElementById("pole_1").value;
+   		var liczbaPole_2 = document.getElementById("pole_2").value;
+   		var liczbaPole_3 = document.getElementById("pole_3").value;
+   		
+   		var mocUrzadzenia_w_Watach = liczbaPole_1.replace(',','.');
+   		var czasPracyUrzadzeniaMinuty = liczbaPole_2.replace(',','.');
+   		var kosztKWh = liczbaPole_3.replace(',','.');
+   		
+   		var zuzycieEnergiiUrzadzeniaWczasie_w_KWh = ((mocUrzadzenia_w_Watach * czasPracyUrzadzeniaMinuty) / 60) / 1000;			
+			var wynikKosztowMinuty = zuzycieEnergiiUrzadzeniaWczasie_w_KWh * kosztKWh;
+   		   		
+   		document.getElementById("wynikMinuty").innerHTML="Koszty pracy urządzenia o mocy " + mocUrzadzenia_w_Watach + "W" + " " + "które pracuje w ciągu " + czasPracyUrzadzeniaMinuty + " " + "Minut" + " " + "wynosi: " + wynikKosztowMinuty + " " + "PLN";
+   	//	window.alert("Koszt pracy urządzenia o mocy= " + mocUrzadzenia_w_Watach + " Wat " + "W czasie " + czasPracyUrzadzeniaMinuty + "minuty" + "wynosi" + wynikKosztow + "Kurwa Mać");
+   		
+   		/*var obliczonePola = Number(pole_1.value) + Number(pole_2.value) + Number(pole_3.value);
+   		document.getElementById("wynik").innerHTML=obliczonePola; 
+   		window.alert("Wynik= " + obliczonePola + " Kurwa Mać"); */
+   	}
+   	
+   	function energiaGodziny() {
+   		var liczbaPole_4 = document.getElementById("pole_4").value;
+   		var liczbaPole_5 = document.getElementById("pole_5").value;
+   		var liczbaPole_6 = document.getElementById("pole_6").value;
+   		
+   		var mocUrzadzenia_w_Watach_Godziny = liczbaPole_4.replace(',','.');
+   		var czasPracyUrzadzeniaGodziny = liczbaPole_5.replace(',','.');
+   		var kosztKWh_Godziny = liczbaPole_6.replace(',','.');
+   		
+    		var zuzycie_Energii_Urzadzenia_W_czasie_w_KWh_Godziny = (mocUrzadzenia_w_Watach_Godziny * czasPracyUrzadzeniaGodziny) / 1000;
+    		var wynikKosztow_Godziny = zuzycie_Energii_Urzadzenia_W_czasie_w_KWh_Godziny * kosztKWh_Godziny;
+    		
+    			document.getElementById("wynikGodziny").innerHTML="Koszty pracy urządzenia o mocy " + mocUrzadzenia_w_Watach_Godziny + " Wat" + " " + "które pracuje w ciągu " + czasPracyUrzadzeniaGodziny + " " + "Godzin" + " " + "wynosi: " + wynikKosztow_Godziny + " " + "PLN";  
+   	//	window.alert("Koszt pracy urządzenia o mocy= " + mocUrzadzenia_w_Watach + " Wat " + "W czasie " + czasPracyUrzadzeniaMinuty + "minuty" + "wynosi" + wynikKosztow + "Kurwa Mać");
+    
+   	}
+   	
+   	function energiaDni() {
+   		var liczbaPole_7 = document.getElementById("pole_7").value;
+   		var liczbaPole_8 = document.getElementById("pole_8").value;
+   		var liczbaPole_9 = document.getElementById("pole_9").value;
+   		
+   		var moc_Urzadzenia_w_Watach_Dni = liczbaPole_7.replace(',','.');
+   		var czas_Pracy_Urzadzenia_Dni = liczbaPole_8.replace(',','.');
+   		var kosztKWh_Dni = liczbaPole_9.replace(',','.');
+   		
+   		var zuzycie_Energii_Urzadzenia_W_czasie_w_KWh_Dni = ((moc_Urzadzenia_w_Watach_Dni * czas_Pracy_Urzadzenia_Dni) * 24) / 1000;  
+    		var wynikKosztow_Dni = zuzycie_Energii_Urzadzenia_W_czasie_w_KWh_Dni * kosztKWh_Dni;
+    		
+    document.getElementById("wynikDni").innerHTML="Koszt pracy urządzenia o mocy " + moc_Urzadzenia_w_Watach_Dni + " Wat" + " w czasie " + czas_Pracy_Urzadzenia_Dni + " dni" + " wynosi: " + wynikKosztow_Dni + " PLN"
+   	}
+   	
+   	function pradNaMoc400() {
+   		var liczbaPole_10 = document.getElementById("pole_10").value;
+    		var naterzenieAmpery = liczbaPole_10.replace(',','.');
+    		var cosFi = 0.8;
+    		var pierwiastekZtrzech = 0.00173205;
+    		var napiecieVolt = 400;
+
+    		var wynikKiloWaty = (((pierwiastekZtrzech * naterzenieAmpery) * napiecieVolt) * cosFi);
+    		document.getElementById("pradNaMocWynik").innerHTML="Moc urządzenia przy napięciu 400 V i natęrzeniu prądu wynoszącym: " + naterzenieAmpery + "A wynosi: " + wynikKiloWaty + "KW"; 
+   	}
+   	
+   	function mocNaPrad400() {
+   		var liczbaPole_11 = document.getElementById("pole_11").value;
+    		var moc = liczbaPole_11 = liczbaPole_11.replace(',','.'); 
+    		var cosFi = 0.8
+    		var pierwiastekZtrzech = 0.00173205
+    		var napiecieVolt = 400
+    		var podzielnikMocy = (pierwiastekZtrzech * napiecieVolt) * cosFi;
+    		var pradWynik = moc / podzielnikMocy;
+    		
+    		document.getElementById("mocNaPradWynik").innerHTML="Moc urządzenia: " + moc + " KW  Napięcie sieci: " + napiecieVolt + " Volt" + " Natężenie wynosi: " + pradWynik + " A";
+   	}
+   	
+   	function mocNaPrad230() {
+   		var liczbaPole_12 = document.getElementById("pole_12").value;
+    		var mocPradJednofazowy = liczbaPole_12.replace(',','.');
+    		var cosFi = 0.8;
+    		var napiecieVolt = 230;
+    		
+    		var pradWynik = mocPradJednofazowy / napiecieVolt;
+    		
+    		document.getElementById("mocNaPrad230Wynik").innerHTML="Przy napięciu " + napiecieVolt + " V" + " i mocy urządzenia wynoszącej: " + mocPradJednofazowy + " W" + " natężenie prądu wynosi: " + pradWynik + " Amperów"; 
+   	}
+   	
+   	function obliczKondensator() {
+   		var liczbaPole_13 = document.getElementById("pole_13").value;
+   		var moc = liczbaPole_13.replace(',','.');
+   		var liczbaPole_14 = document.getElementById("pole_14").value;
+   		var wspolczunnik = liczbaPole_14.replace(',','.');
+    		var cosFi = 0.8;
+    		
+    		var pojemnosc = (wspolczunnik * moc) * cosFi;
+    
+    		document.getElementById("obliczKondensatorWynik").innerHTML="Pojemność kondensatora wynosi: " + pojemnosc + " uF";
+   	}	
